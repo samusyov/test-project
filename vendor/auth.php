45f5cc4ce1ee8ca21412f083e5f591b4e547f66e
Иван Самусёв <samusyov.spam@gmail.com>
2022-12-01 13:15:02 +0300

    file root changes

--- vendor/auth.php
+++ vendor/auth.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if (file_exists('C:\xampp\htdocs\test-project\db\file.json'))
+if (file_exists('../db/file.json'))
 {
     if (isset($_POST['login']) && isset($_POST['password']))
     {
@@ -17,7 +17,7 @@ else
 
 function AuthLoginAndPass($login, $password): array
 {
-    $json_arr = json_decode(file_get_contents('C:\xampp\htdocs\test-project\db\file.json'), true);
+    $json_arr = json_decode(file_get_contents('../db/file.json'), true);
     if (is_array($json_arr) || is_object($json_arr))
     {
         foreach ($json_arr as $one)
